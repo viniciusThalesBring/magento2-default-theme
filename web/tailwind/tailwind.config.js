671398444a8db5db3ddde58c11de23cb4e167148
Vinai Kopp <vinai@netzarbeiter.com>
2022-12-07 13:16:25 +0100

    #543 Apply font config via tailwind config

--- web/tailwind/tailwind.config.js
+++ web/tailwind/tailwind.config.js
@@ -21,6 +21,9 @@ module.exports = hyvaModules.mergeTailwindConfig({
         '2xl': '1536px' // => @media (min-width: 1536px) { ... }
 
       },
+      fontFamily: {
+        sans: ["Segoe UI", "Helvetica Neue", "Arial", "sans-serif"]
+      },
       colors: {
         primary: {
           lighter: colors.blue['300'],
